8127247d304cef02f94a685176ea6346c2778dca
Oungseik <mhemaungthuwin@gmail.com>
2023-06-17 18:50:14 +0630

    chore: setup development server with nodemon

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 // @ts-check
 const express = require("express");
+const { log } = require("./logger");
 
 const app = express();
+app.use("/", express.static(`${process.cwd()}/dist`));
 
 module.exports = app;
