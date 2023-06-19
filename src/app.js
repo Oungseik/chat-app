53558cd191a217d5870787a62f27b1001d8f131c
Oungseik <mhemaungthuwin@gmail.com>
2023-06-19 19:03:02 +0630

    feat: create sending form

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 // @ts-check
 require("express-async-error");
-// const errorHandler = require("./middlewares/error-handler");
+const errorHandler = require("./middlewares/error-handler");
 const registerRouter = require("./routers/register");
 
 const express = require("express");
 const cookieParse = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use("/", express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParse());
 
 app.use("/register", registerRouter);
 
-// app.use(errorHandler);
+app.use(errorHandler);
 
 module.exports = app;
--- src/routers/register.js
+++ src/routers/register.js
@@ -1,11 +1,24 @@
 //@ts-check
 const { Router } = require("express");
 const { getViews } = require("../lib/utils");
+const { BadRequestError } = require("../lib/errors");
 
 const router = Router();
 
+const registerFile = getViews("register", "index.html");
+
 router.get("/", (_request, response) => {
-  response.sendFile(getViews("register", "index.html"));
+  response.sendFile(registerFile);
+});
+
+router.post("/", (request, response) => {
+  const { username, email, password, passwordConfirm } = request.body;
+
+  if (password !== passwordConfirm) {
+    return response.status(400).sendFile(registerFile);
+  }
+  console.log(username, email, password, passwordConfirm);
+  response.redirect("/login");
 });
 
 module.exports = router;
